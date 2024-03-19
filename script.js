e3826029d01934f4efb8c6b62e984918253e883b
trent <trntannan1@gmail.com>
2024-03-19 20:53:01 +1300

    light mode adjtment

--- script.js
+++ script.js
@@ -52,4 +52,4 @@ document.addEventListener("DOMContentLoaded", function() {
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("light-mode")
-})
+});
